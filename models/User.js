d87fd20e60dabfffc651c70e3511cb9c05cc3ba6
Viet Tai <viettaixca123@gmail.com>
2023-07-14 14:03:34 +0700

    Model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name must be provided"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Password must be provided"],
+    min: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  verificationToken: String,
+  isVerified: { type: Boolean, default: false },
+  verified: Date,
+  passwordToken: String,
+  passwordTokenExpirationDate: Date,
+});
+
+UserSchema.pre("save", async function () {
+  if (this.password.length > 50) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePWD = async function (pwd) {
+  return await bcrypt.compare(pwd, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
