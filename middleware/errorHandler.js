a588a624a158c4a9b208f4ffa6ec7da7ec2a0e77
Viet Tai <viettaixca123@gmail.com>
2023-07-16 13:38:01 +0700

    Update middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,32 +3,32 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong try again later",
+    message: err.message || "Một số lỗi máy chủ, vui lòng thử lại sau",
   };
   if (err.name === "ValidationError") {
-    customError.msg = Object.values(err.errors)
+    customError.message = Object.values(err.errors)
       .map((item) => item.message)
       .join(",");
     customError.statusCode = 400;
   }
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(
+    customError.message = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
     customError.statusCode = 400;
   }
   if (err.name === "CastError") {
     if (typeof err.value === "object") {
-      customError.msg = `No item found with id : ${
+      customError.message = `Không tìm thấy với id : ${
         Object.values(err.value)[0]
       }`;
     } else {
-      customError.msg = `No item found with id : ${err.value}`;
+      customError.message = `Không tìm thấy với id : ${err.value}`;
     }
     customError.statusCode = 404;
   }
 
-  return res.status(customError.statusCode).json({ msg: customError.msg });
+  return res.status(customError.statusCode).json({ message: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
