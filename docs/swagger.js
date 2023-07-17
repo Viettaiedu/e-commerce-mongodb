eba7444a5b94bfe4c4f543cb6127fd4d55a9cd10
Viet Tai <viettaixca123@gmail.com>
2023-07-17 14:50:46 +0700

    fix bug: swagger url

--- docs/swagger.js
+++ docs/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:4000/api/v1",
+        url: "https://viettai-ecommerce-api-mongodb.vercel.app",
         description: "Local server",
       },
     ],
