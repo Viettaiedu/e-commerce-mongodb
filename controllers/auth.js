bfce061fdb09f2d5f73584815db73cd81e0730e6
Viet Tai <viettaixca123@gmail.com>
2023-07-22 15:28:17 +0700

    Cập nhật lại Auth Controller, sửa lại một vài nơi để viết unit testing đầu tiên auth.test.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+} = require("../errors");
 const { attachCookiesResponse } = require("../utils/jwt");
-const createHash = require("../utils/createHash");
 const createTokenUser = require("../utils/createTokenUser");
 const sendVerificationEmail = require("../utils/sendVerification");
 const sendResetPasswordEmail = require("../utils/sendResetPasswordEmail");
 const Token = require("../models/Token");
 const { generateToken } = require("../utils/generateToken");
+const { comparePWD } = require("../utils/bcrypt");
+const hashString = require("../utils/createHash");
 
 const register = async (req, res) => {
   const { email } = req.body;
@@ -17,7 +22,7 @@ const register = async (req, res) => {
   if (isFirstUser) {
     req.body.role = "admin";
   }
-  const verificationToken = generateToken();
+  const verificationToken = await generateToken();
   req.body.verificationToken = verificationToken;
   const user = await User.create({ ...req.body });
   // const origin = "http://localhost:4000";
@@ -27,15 +32,17 @@ const register = async (req, res) => {
   //   verificationToken,
   //   origin,
   // });
-  res.status(StatusCodes.CREATED).json({ message: "Đăng ký thành công!" });
+  res.status(StatusCodes.CREATED);
+  res.json({ message: "Đăng ký thành công!" });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) throw new BadRequestError(`Email không hợp lệ!`);
+
   const isMatchPWD = await comparePWD(password, user.password);
   if (!isMatchPWD) throw new BadRequestError(`Password không hợp lệ!`);
-  const tokenUser = createTokenUser(user);
+
   if (!user.isVerified) {
     const origin = "http://localhost:4000";
     await sendVerificationEmail({
@@ -48,6 +55,8 @@ const login = async (req, res) => {
       "Xin vui lòng xác nhận email để có thể đăng nhập vào hệ thông!"
     );
   }
+
+  const tokenUser = createTokenUser(user);
   let refreshToken = "";
   const existingToken = await Token.findOne({ userId: user._id });
   if (existingToken) {
@@ -71,7 +80,8 @@ const login = async (req, res) => {
 const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
   const user = await User.findOne({ email });
-  if (!user) throw new UnauthenticatedError(`Xác minh email thất bại!`);
+  if (!user) throw new BadRequestError(`Email không hợp lệ`);
+
   if (user.verificationToken !== verificationToken) {
     throw new UnauthenticatedError("Xác minh email thất bại!");
   }
@@ -79,13 +89,15 @@ const verifyEmail = async (req, res) => {
   user.verified = Date.now();
   user.verificationToken = "";
   await user.save();
-  res.status(StatusCodes.OK).json({ message: "Xác minh email thành công!" });
+  res.status(StatusCodes.OK);
+  res.json({ message: "Xác minh email thành công!" });
 };
 
 const forgotPassword = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
-  if (!user) throw new UnauthenticatedError("Xác minh email thành công!");
+  if (!user) throw new BadRequestError("Email không hợp lệ!");
+
   const origin = "http://localhost:3000";
   const passwordToken = generateToken();
   await sendResetPasswordEmail({
@@ -96,10 +108,11 @@ const forgotPassword = async (req, res) => {
   });
   const tenMinutes = 60 * 1000 * 10;
   const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
-  user.passwordToken = createHash(passwordToken);
+  user.passwordToken = hashString(passwordToken);
   user.passwordTokenExpirationDate = passwordTokenExpirationDate;
   await user.save();
-  res.status(StatusCodes.OK).json({
+  res.status(StatusCodes.OK);
+  res.json({
     message: "Vui lòng kiểm tra email của bạn để đặt lại mật khẩu của bạn!",
   });
 };
@@ -115,19 +128,23 @@ const resetPassword = async (req, res) => {
     throw new BadRequestError("Passowrd cần ít nhất là 6 kí tự");
   }
   const user = await User.findOne({ email });
-  if (user) {
-    const currentDate = new Date();
-    if (
-      user.passwordToken === createHash(token) &&
-      user.passwordTokenExpirationDate > currentDate
-    ) {
-      user.password = password;
-      user.passwordToken = null;
-      user.passwordTokenExpirationDate = null;
-      await user.save();
-    }
+  if (!user) {
+    throw new NotFoundError("Người dùng không tồn tại");
   }
-  return res.status(200).json({ message: "Đặt lại mật khẩu thành công!" });
+  const currentDate = new Date(Date.now());
+
+  if (
+    user.passwordToken === (await hashString(token)) &&
+    user.passwordTokenExpirationDate > currentDate
+  ) {
+    user.password = password;
+    user.passwordToken = null;
+    user.passwordTokenExpirationDate = null;
+    await user.save();
+    res.status(200);
+    return res.json({ message: "Đặt lại mật khẩu thành công!" });
+  }
+  throw new UnauthenticatedError("Không có quyền đặt lại mật khẩu!");
 };
 
 const logout = async (req, res) => {
@@ -140,7 +157,8 @@ const logout = async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + 1000),
   });
-  res.status(StatusCodes.OK).json({ message: "Đăng xuất thành công!" });
+  res.status(StatusCodes.OK);
+  res.json({ message: "Đăng xuất thành công!" });
 };
 // entire user [testing]
 module.exports = {
